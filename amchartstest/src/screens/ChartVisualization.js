764b604739f754f871d7e7847c8d96c490b24686
Gustavo-Hernandez <47768780+Gustavo-Hernandez@users.noreply.github.com>
2020-03-09 11:22:47 -0600

    ChartVisualization Screen Created

---
+++ amchartstest/src/screens/ChartVisualization.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class ChartVisualization extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            <div>This is a Chart Visualization</div>
+        );
+    }
+}
+ 
+export default ChartVisualization;
